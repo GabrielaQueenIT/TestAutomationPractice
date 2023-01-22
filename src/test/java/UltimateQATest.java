1ba74e0d2f3b298475e8aea51341c28b37bb9ec9
Gabriela Krol <gabadel93@gmail.com>
2023-01-22 13:28:22 +0100

    First project created

---
+++ src/test/java/UltimateQATest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class UltimateQATest {
+    String projectLocation = System.getProperty("user.dir");
+    WebDriver driver;
+
+    //hermetyzacja
+    @Test
+    public void testOne(){
+        System.out.println(projectLocation);
+        System.setProperty("webdriver.chrome.driver", projectLocation + "/resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://ultimateqa.com/simple-html-elements-for-automation/");
+//        driver.findElement(By.id(""));
+//        driver.close();
+    }
+}
